295f3e6ec539f2dd2b2b0e2178ddc2130161e0a7
Vikas Reddy <vikas@framediff.com>
2026-08-12 15:43:13 -0600

    Refresh dependency and build architecture

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from "node:url";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import { framediffDev } from "../../packages/framediff/vite-plugin";
+import { framediffDev } from "../../packages/framediff/vite-plugin.ts";
 
 export default defineConfig({
   plugins: [sveltekit(), framediffDev()],
